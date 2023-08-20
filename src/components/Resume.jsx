f33446002023aaf8fd151aa8c374a18489d7d61c
Nick <93883375+nickthome@users.noreply.github.com>
2023-08-20 12:36:19 -0500

    Title change and resume download change

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ import './styles/Resume.css';
 function Resume() {
     return (
         <div className="app-container">
-            <a className="download-link" href="./NickThomeResume2023.pdf" download="NickThomeResume.pdf">
+            <a className="download-link" href="./NickThomeResume2023.pdf" download="NickThomeResume2023.pdf">
                 <h5>Resume</h5>
             </a>
         </div>
